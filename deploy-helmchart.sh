000ed5f00228d1ae7d9effea8cfec7097cad943e
Gianni Carafa <gianni.carafa@srf.ch>
2023-05-19 10:50:44 +0200

    update helm deployment

--- deploy-helmchart.sh
+++ deploy-helmchart.sh
@@ -1,3 +1,3 @@
 helm lint charts/haraka/ && \
-helm package -d helm-repo charts/haraka/ \
+helm package -d helm-repo charts/haraka/ && \
 helm repo index helm-repo --url https://haraka-helm.kubero.dev
